f05c2827e8843450537ba4056a2e1536daeeca39
Nicolás Meneses <felipemenesespeinado5@gmail.com>
2024-06-19 16:47:20 -0500

    navbar y card

---
+++ apptodo/src/Example.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Example() {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">ToDo</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Crear tareas</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Pendientes</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Ver tareas
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><hr className="dropdown-divider"></hr></li>
+                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" aria-disabled="true">Expiradas</a>
+                            </li>
+                        </ul>
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Ingrese el nombre de la tarea" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Buscar</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+            <div className="card" style={{ width: '18rem' , margin: '20px' }}>
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Tarea</h5>
+                    <p className="card-text">Descripción de la tarea.</p>
+                    <a href="#" className="btn btn-primary">Ver tarea</a>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Example;
